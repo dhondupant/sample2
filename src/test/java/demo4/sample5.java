fdc829a0cdcd110cc40c5dbcf44ea8537a257428
Dell <Dell@DESKTOP-54KFCAF>
2023-09-11 02:51:16 -0700

    hjtfyhjftyj

---
+++ src/test/java/demo4/sample5.java
@@ -0,0 +1,17 @@
+package demo4;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class sample5 {
+	
+
+	@Test
+	public void TC3()
+	{
+		Reporter.log("sdfassd");
+	}
+	
+
+
+}
